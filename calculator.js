0641bf9a0a1111f5366beb6f08a46efafb85d048
gomeschian <celusher@g.ucla.edu>
2022-05-17 21:46:56 -0700

    Make functions for addition, subtraction, multiplcation, and division.

---
+++ calculator.js
@@ -0,0 +1,17 @@
+const add = function(num1, num2) {
+	return num1 + num2;
+};
+
+const subtract = function(num1, num2) {
+	return num1 - num2;
+};
+
+const multiply = function(number1, number2) {
+
+  return number1 * number2;
+
+};
+
+const divide = function(number1, number2) {
+	return (number1/number2) + (number1 % number2);
+};
