e069d43605d894bf99b42eef2e0b5fc2d681e793
gomeschian <celusher@g.ucla.edu>
2022-05-17 21:53:59 -0700

    Add function to call operations.

--- calculator.js
+++ calculator.js
@@ -15,3 +15,26 @@ const multiply = function(number1, number2) {
 const divide = function(number1, number2) {
 	return (number1/number2) + (number1 % number2);
 };
+
+const operate = function(operator, number1, number2) {
+
+  if (operator === "+")
+  {
+    return add(number1, number2);
+  }
+  
+  if (operator === "-")
+  {
+    return subtract(number1, number2);
+  }
+
+  if (operator === "*")
+  {
+    return multiply(number1, number2);
+  }
+
+  if (operator === "/")
+  {
+    return divide(number1, number2);
+  }
+};
